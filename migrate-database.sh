ca9eea90fec5583337239179184f67b79c55f1ca
Franco A <franco.alabastro@gmail.com>
2017-07-25 12:34:58 -0400

    countdown

--- migrate-database.sh
+++ migrate-database.sh
@@ -6,5 +6,5 @@ if [ ! -f app/Config/Schema/schema.php ]; then
 	cake schema generate
 fi
 
-echo "Migrating database 'cake schema create' ..."
+echo "Migrating database 'cake schema create in 3 2 1' ..."
 cake schema create
