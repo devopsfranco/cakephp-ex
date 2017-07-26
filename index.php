678c220f94686f12bd84768527eb8516d6714942
Franco A <franco.alabastro@gmail.com>
2017-07-26 15:22:21 -0400

    for once in my life

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Requests collector.
  *
- * I don't like PHP
+ * gonna make a change
  *  This file collects requests if:
  *	- no mod_rewrite is available or .htaccess files are not supported
  *  - requires App.baseUrl to be uncommented in app/Config/core.php
